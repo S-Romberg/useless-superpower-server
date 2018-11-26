5a5e92c3e410b6e517b0f007958bbfed392b4e12
S-Romberg <spencer.romberg@gmail.com>
2018-11-26 10:34:21 -0700

    changed port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const queries = require('./queries')
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 var data = [{id: 1, superpower: "Transform into yourself", name:"Conner", img:""}, {id: 2, superpower: "Contract any disease at will", name:"Greg", img:""}, {id: 3, superpower: "Disappear when people aren't around", name:"Beef", img:""}, {id: 4, superpower: "Move as slow as a sloth", name:"Ryan", img:""}, {id: 5, superpower: "Ability to sneeze at will", name:"Kate", img:""}, {id: 6, superpower: "Teleport one body part", name:"Phill", img:""}]
 
